5ddd7fb0466afa121efee089bd59ee6da4395079
MaiaRT46 <maia.rt.46@gmail.com>
2017-02-28 16:24:48 -0500

    primer comentario

---
+++ JS/prueba1-branch.js
@@ -0,0 +1,7 @@
+
+  var records = document.getElementById('records') ;
+//alert ("basio "  + auxilio +"calra");if (auxilio !="" && apellido != "" && dni!=""&&adress!="") {
+function hola(){
+console.log("Hola Mundo");
+};
+hola();
