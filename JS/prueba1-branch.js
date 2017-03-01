acd98eecb97d514ef420bcc0ecbac6d204ad1526
FiorellaQA <fiorella_90_17@hotmail.com>
2017-03-01 10:15:14 -0500

    probando merge

--- JS/prueba1-branch.js
+++ JS/prueba1-branch.js
@@ -2,6 +2,6 @@
   var records = document.getElementById('records') ;
 //alert ("basio "  + auxilio +"calra");if (auxilio !="" && apellido != "" && dni!=""&&adress!="") {
 function hola(){
-console.log("Hola Mundo");
+console.log("Adios mundo.");
 };
 hola();
